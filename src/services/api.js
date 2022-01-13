f9498bd01d2aa3fafdcbe2a7448157b0351858ba
matheusifz <matheusifz@gmail.com>
2022-01-13 16:36:13 -0300

    feat: create fetch function

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 const API_KEY = "66366d0ce4ff8912b6a34594a36c45c7";
 const API_BASE = "https://api.themoviedb.org/3";
 
+const fetchApi = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+};
+
 export default {
   getHomeList: async () => {
     return [
